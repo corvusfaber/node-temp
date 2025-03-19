b88159f8467a93659186f2af8e9bf910ed69ba06
Malcolm Fraser <cfaber2004@yahoo.com>
2025-03-19 09:27:54 +0200

    Remove unecassary files.

--- compile.sh
+++
@@ -1,7 +0,0 @@
- #!/bin/bash
-
-docker build -t mf-node-app-template:latest .
-
-docker tag mf-node-app-template:latest malcolmcfraser/mf-node-app-template:1.1
-
-docker push malcolmcfraser/mf-node-app-template:1.1
