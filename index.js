91b2dadda5226c3c202fadd361df97c38236f2c0
Malcolm Fraser <cfaber2004@yahoo.com>
2025-04-04 13:40:52 +0200

    Increased the timeout.

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ async function initializeDatabase (){
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE
+        database: process.env.MYSQL_DATABASE,
+        connectTimeout: 10000
     });
     await connection.execute(
         `CREATE TABLE IF NOT EXISTS users (
