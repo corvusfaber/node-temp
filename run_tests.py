a287c563aa0d08787f04470fd0a1c44680d792d5
Malcolm Fraser <cfaber2004@yahoo.com>
2025-05-06 13:55:10 +0200

    Added ingress and hpa to the python test launch.

--- run_tests.py
+++ run_tests.py
@@ -12,6 +12,8 @@ def deploy_app():
     subprocess.run(["kubectl", "apply", "-f", "./node-app-template-artifacts/mysql-statefulset.yaml"], check=True) # path to statefulset
     time.sleep(10) # Wait for mysql stateful set and services before deploying the app.
     subprocess.run(["kubectl", "apply", "-f", "./node-app-template-artifacts/node-app.yaml"], check=True)# path to node-deployment-template
+    subprocess.run(["kubectl", "apply", "-f", "./node-app-template-artifacts/node-app-ingress.yaml"], check=True)
+    subprocess.run(["kubectl", "apply", "-f", "./node-app-template-artifacts/node-app-hpa.yaml"], check=True)
     subprocess.run(["kubectl", "wait", "--for=condition=available", "--timeout=120s", "deployment/mf-node-app"], check=True)
     
     # Wait for the pods to be ready
